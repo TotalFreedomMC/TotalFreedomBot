2d2bde604d1f462f92fe39f576c44c989ae7d138
Elmon11 <elmon11bussiness@gmail.com>
2020-12-02 22:45:40 +0100

    Update telnet.py

--- telnet.py
+++ telnet.py
@@ -8,7 +8,7 @@ class telnet:
         self.port = port
         self.username = username
         self.password = password
-        
+
     def connect(self, *username):
         self.session = Telnet(self.ip, self.port)
         for x in username:
@@ -16,7 +16,7 @@ class telnet:
 
         if b'Username:' in self.session.read_until(b'Username:', timeout = 3):
             time.sleep(0.5)
-            self.session.write(bytes(self.username, 'ascii') + b"\r\n")              
+            self.session.write(bytes(self.username, 'ascii') + b"\r\n")
         if b'Password:' in self.session.read_until(b'Password:', timeout = 3):
             time.sleep(0.5)
             self.session.write(bytes(self.password, 'ascii') + b"\r\n")
