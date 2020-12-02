dc0a0a2a7f3f058dd5fdd7acea385eec929e4833
Elmon11 <elmon11bussiness@gmail.com>
2020-12-02 23:18:47 +0100

    Update moderation.py

--- commands/moderation.py
+++ commands/moderation.py
@@ -10,6 +10,7 @@ muted_role_id = 769659653121900546
 
 class Moderation(commands.Cog):
     def __init__(self, bot):
+        """Defines bot object and mod role id on construction"""
         self.bot = bot
         self.moderator_role_id = 769659653129896023
 
